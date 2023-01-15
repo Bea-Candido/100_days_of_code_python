27a4da52fdb2c23a605592952e25a06d715864ae
BeaPc <developer.beacandido@gmail.com>
2023-01-15 20:00:52 -0300

    feat: new class

---
+++ 2.day/primitive_data_types.py
@@ -0,0 +1,12 @@
+#Data Types
+
+# ++ String
+
+# print("Hello"[0])
+# programmers always start counting 
+# "123" it's not a number , it's a string 
+# other example: "123"+"345" = 123456
+
+#Float 
+3.14159 
+
