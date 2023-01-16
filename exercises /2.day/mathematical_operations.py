5f1dd3817b22a031d326cf5d4f149666b30cb518
BeaPc <developer.beacandido@gmail.com>
2023-01-16 09:42:22 -0300

    feat: new class

---
+++ exercises /2.day/mathematical_operations.py
@@ -0,0 +1,26 @@
+#  ++ Operations
+# 3+5
+# 7-3
+# 3*2
+# 6/3 
+# # (when you re  dividing things you actually always end up with a floating point number  )
+
+# 2**2
+
+# In code we have a level of priority (as math)
+# PEMDAS
+# 1.()
+# 2. **
+# 3. * or / 
+# 4. + or 
+
+print(3*3 + 3/3 -3 )
+
+# for result 3
+
+print(3*(3+3)/3-3)
+
+
+
+
+
