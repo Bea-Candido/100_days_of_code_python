2f6bcfaaa76833c96dc0bd3c39ae811e5abc4ccd
BeaPc <developer.beacandido@gmail.com>
2023-01-16 08:58:07 -0300

    feat: first exercise

---
+++ exercises /2.day/data_types.py
@@ -0,0 +1,20 @@
+two_digit_number = input("Type a two digit numbers:")
+
+first_number = int (two_digit_number[0])
+
+second_number = int(two_digit_number[1])
+
+two_digit_number = first_number + second_number 
+
+print(two_digit_number)
+
+# # Another solution if we didn't have the first line:
+
+# print("Type a two digit number")
+# first_number= int(input())
+# second_number= int(input())
+
+# values= first_number + second_number 
+
+# print(values)
+
