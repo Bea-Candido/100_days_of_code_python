b4716ba40eddb5216e6a203e07ba83cc8960f843
BeaPc <developer.beacandido@gmail.com>
2023-01-16 08:17:29 -0300

    feat: add new data types

--- 2.day/primitive_data_types.py
+++ 2.day/primitive_data_types.py
@@ -7,6 +7,22 @@
 # "123" it's not a number , it's a string 
 # other example: "123"+"345" = 123456
 
-#Float 
+# ++ Integer 
+# print(369+121518)
+
+# In uk , us and here in brazil we'd like to put commas in between the thousands.
+# ex: 342,654,896
+# In Python we can replace those commas simply with underscores and it will be interpreted.
+# ex: 342_654_896
+
+# ++ Float 
 3.14159 
+# Decimal numbers 
+
+# ++ Boolean 
+
+# True or False
+
+
+
 
