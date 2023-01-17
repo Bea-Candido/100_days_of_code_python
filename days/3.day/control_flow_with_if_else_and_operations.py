456ec9ae3864a3a6364f5fbef49261754855e554
BeaPc <developer.beacandido@gmail.com>
2023-01-16 23:17:21 -0300

    feat: first class

---
+++ days/3.day/control_flow_with_if_else_and_operations.py
@@ -0,0 +1,29 @@
+# +++ Conditional if / else
+
+# if condition:
+#     do this 
+# else:
+#     do this    
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+   print("You can ride the rollercoaster!")
+else:
+   print("Sorry, you have to grow taller before you can ride")    
+
+# In Python, the spacing and indentation is really important!
+
+# +++ Comparison operators 
+#  >     greater than 
+#  <     less than
+# >=     greater than or equal to
+# <=     less than or equal to 
+# ==     equal 
+# !=     not equal
+
+ 
+
+
+
