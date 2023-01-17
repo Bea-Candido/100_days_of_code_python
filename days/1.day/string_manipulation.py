7aa55bac83d1512ffc8a80477875191f96676c58
BeaPc <developer.beacandido@gmail.com>
2023-01-16 23:15:26 -0300

    refactor: organization of days

---
+++ days/1.day/string_manipulation.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+
+print("Hello world!\nHello world\nHello world")
+
+print("Hello" + "Angela")
+print("Hello"+ " " + "Angela")
+
+# Spaces in Python it's important 
+
---
+++ days/1.day/variables.py
@@ -0,0 +1,15 @@
+# 1-
+name = input("What is your name?")
+print(name)
+
+# 2-
+name = "Jack"
+print(name)
+
+name="Angela"
+print(name)
+
+# 3-
+name= input("What is your name?")
+length= len(name)
+print(length)
