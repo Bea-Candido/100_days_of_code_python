0027c99753526f65c271f508f3a2d06b7960dfa2
BeaPc <developer.beacandido@gmail.com>
2023-03-22 10:42:23 -0300

    feat: pizza exercise started

---
+++ exercises /3.day/pizza_order_practice.py
@@ -0,0 +1,4 @@
+print("Welcome to python pizza delivery")
+size = input("What size pizza do you want? S,M, or L")
+add_pepperoni= input("DO you want pepperoni?Y or N")
+extra_cheese = input ("Do you want extra cheese? Y or N")
